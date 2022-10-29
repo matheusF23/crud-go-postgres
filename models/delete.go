0e1bb19c47d3709a93ec70abee542564b39f5779
Matheus Figueiredo <figueiredo.matheus06@gmail.com>
2022-10-29 15:52:12 -0300

    fix: query of delete

--- models/delete.go
+++ models/delete.go
@@ -8,7 +8,7 @@ func Delete(id int64) (int64, error) {
 		return 0, err
 	}
 	defer conn.Close()
-	res, err := conn.Exec(`UPDATE FROM todos WHERE id=$1`, id)
+	res, err := conn.Exec(`DELETE FROM todos WHERE id=$1`, id)
 	if err != nil {
 		return 0, err
 	}
